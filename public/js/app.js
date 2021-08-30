9caac532fd3672313b77b6afaa524361ee8d7dca
jiteshrock2019 <jiteshjoshi583@gmail.com>
2021-08-30 13:51:42 +0530

    app setup for deploy

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,7 @@ weatherFrom.addEventListener('submit', (e) => {
 
     console.log(location)
     // if(location.toString()){
-        fetch(`http://localhost:3000/weather?address=${location}`).then( (res) => {
+        fetch(`/weather?address=${location}`).then( (res) => {
         res.json().then( (data) => {
             if(data.error){
                 messageOne.textContent = data.error
